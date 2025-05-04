ace2533d3b67c9f4e37513a9992f967b5c0becd0
lunasamueel <san.samueel@gmail.com>
2025-05-04 11:49:50 -0300

    refactor: testes de unidade

--- mini_twitter/tests/test_post.py
+++ mini_twitter/tests/test_post.py
@@ -31,10 +31,10 @@ class PostTests(APITestCase):
 
     def test_like_post(self):
         response = self.client.post(f'/api/posts/{self.post.id}/like/')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Like.objects.count(), 1)
 
     def test_duplicate_like(self):
         self.client.post(f'/api/posts/{self.post.id}/like/')
         response = self.client.post(f'/api/posts/{self.post.id}/like/')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
